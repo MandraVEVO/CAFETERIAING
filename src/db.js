a8e15bae297986c7d9f8b48b434d67f3ed26ac80
Diego Raul Hernandez Cardeña <paratrabajosquesenecesiten15@gmail.com>
2024-11-12 11:01:09 -0600

    update db.js

--- src/db.js
+++ src/db.js
@@ -3,16 +3,14 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+// Configura Sequelize usando DATABASE_URL para entornos en Heroku
 const databaseUrl = process.env.DATABASE_URL || 
   `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 const sequelize = new Sequelize(databaseUrl, {
   dialect: 'postgres',
   dialectOptions: {
-    ssl: process.env.DATABASE_URL ? {
-      require: true,
-      rejectUnauthorized: false
-    } : false
+    ssl: process.env.DATABASE_URL ? { require: true, rejectUnauthorized: false } : false
   }
 });
 
