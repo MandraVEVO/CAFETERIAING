1e64240100075877aa7166faed6b121e8110d29a
Diego Raul Hernandez Cardeña <paratrabajosquesenecesiten15@gmail.com>
2024-11-12 16:30:59 -0600

    update

--- src/app.js
+++ src/app.js
@@ -7,23 +7,14 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Esta es la raiz de mi Cafeteria');
-});
-
-// Conectar a la base de datos y luego iniciar el servidor
-sequelize.authenticate()
-  .then(() => {
-    console.log('Conectado a la base de datos.');
-    app.listen(port, () => {
-      console.log('Server is running on http://localhost:' + port);
-    });
-  })
-  .catch(err => {
-    console.error('No se pudo conectar a la base de datos:', err);
-  });
+app.get('/',(req,res)=> {
+    res.send('Esta es la raiz de mi Cafeteria');
+})
 
 routes(app);
+
+app.listen(port, ()=>{
+    console.log('Server is running on http://localhost:'+port); 
+});
